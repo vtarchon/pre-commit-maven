7722ad63c6113e7252983668274f960cf3d958e3
Tom Kuo <1598170+vtarchon@users.noreply.github.com>
2024-05-16 15:56:47 -0400

    Update generic_main.py

--- pre_commit_maven/utils/generic_main.py
+++ pre_commit_maven/utils/generic_main.py
@@ -7,5 +7,7 @@ def execute(goals: list, cwd: str, maven_helper=maven):
     execution_result = maven_helper.execute(goals, cwd)
     if execution_result.return_code != 0:
         maven_helper.print_error(execution_result)
-
+    else: 
+        for line in execution_result.stdout.splitlines():
+            print_fn(f"{line}")
     return execution_result.return_code
